16113a75433edc703632350ab43c9092d7b7dc05
Arsénio Miguel Santos Costa <arseniocosta@med.uminho.pt>
2019-08-14 12:31:17 +0100

    - [WIP] resumee

--- src/assets/js/front/index.js
+++ src/assets/js/front/index.js
@@ -14,7 +14,7 @@ window.jQuery(($) => {
 
   let defOpts = Object.assign({}, {
     selector: 'div'
-    , inclass:  ''
+    , inclass: ''
     , outclass: ''
   });
 
@@ -28,20 +28,20 @@ window.jQuery(($) => {
     new window.Waypoint.Inview({
       element: $(opts.selector)
       , enter: function (dir) {
-          // console.log('Enter fired for', this.element , dir);
-          this.element.removeClass(opts.outclass).addClass(opts.inclass);
-        }
+        // console.log('Enter fired for', this.element , dir);
+        this.element.removeClass(opts.outclass).addClass(opts.inclass);
+      }
       , entered: function (dir) {
-          // noty({text: 'Entered fired with direction ' + dir, type: 'information'});
-        }
+        // noty({text: 'Entered fired with direction ' + dir, type: 'information'});
+      }
       , exit: function (dir) {
-          // noty({text: 'Exit fired with direction ' + dir, type: 'notification'});
-          // console.log('exit() for ', this.element , dir);
-        }
+        // noty({text: 'Exit fired with direction ' + dir, type: 'notification'});
+        // console.log('exit() for ', this.element , dir);
+      }
       , exited: function (dir) {
-          // console.log('Exited fired for ', this.element , dir);
-          this.element.removeClass(opts.inclass).addClass(opts.outclass);
-        }
+        // console.log('Exited fired for ', this.element , dir);
+        this.element.removeClass(opts.inclass).addClass(opts.outclass);
+      }
       , offset: opts.offset || '-50%'
     });
 
@@ -62,7 +62,7 @@ window.jQuery(($) => {
     let dataRoot = w.location.origin + '/data/';
 
     // Examine the text in the response
-    function status (r) {
+    function status(r) {
       if (r.status >= 200 && r.status < 300) {
         return Promise.resolve(r);
       } else {
@@ -72,33 +72,33 @@ window.jQuery(($) => {
     }
 
     // Parse response text into javascript object
-    function json (r) {
+    function json(r) {
       let contentType = r.headers.get('content-type');
       if (contentType && contentType.includes('application/json')) {
         return r.json();
-      }else if (contentType && contentType.includes('text/plain')) {
+      } else if (contentType && contentType.includes('text/plain')) {
         return JSON.parse(r);
       }
       throw new TypeError('Oops, we haven\'t got JSON!');
     }
 
     // Get response text
-    function text (r) {
+    function text(r) {
       return r.text();
     }
 
     // Fetch animations configuration
     let AnimationsConfig = fetch(dataRoot + 'animations.json')
-      .then(status)
-      .then(json)
-      .then(function (lo) {
-        // console.log('Fetch Request succeeded with JSON response (', typeof lo, '): [', lo, ']');
-        return Promise.resolve(lo.animations);
-      })
-      .catch(function (err) {
-        console.warn('Failed to fetch DATA: [', err, ']');
-        return Promise.reject(err);
-      });
+    .then(status)
+    .then(json)
+    .then(function (lo) {
+      // console.log('Fetch Request succeeded with JSON response (', typeof lo, '): [', lo, ']');
+      return Promise.resolve(lo.animations);
+    })
+    .catch(function (err) {
+      console.warn('Failed to fetch DATA: [', err, ']');
+      return Promise.reject(err);
+    });
 
     // Enable animations on elements
     let AnimationsEnabled = AnimationsConfig.then(function (loAnimations) {
@@ -133,33 +133,53 @@ window.jQuery(($) => {
 
   (function () {
     $.noty.defaults = {
-      layout:         'topRight'
-      , theme:        'defaultTheme'     // or relax
-      , type:         'success'          // alert, success, error, warning, information, notification
-      , text:         ''                 // [string|html] can be HTML or STRING
-      , dismissQueue: true               // [boolean] If you want to use queue feature set this true
-      , force:        false              // [boolean] adds notification to the beginning of queue when set to true
-      , maxVisible:   8                  // [integer] you can set max visible notification count for dismissQueue true option,
-      , template:     '<div class="noty_message"><span class="noty_text"></span><div class="noty_close"></div></div>'
-      , timeout:      5000               // [integer|boolean] delay for closing event in milliseconds. Set false for sticky notifications
-      , progressBar:  true               // [boolean] - displays a progress bar
-      , buttons:      false              // [boolean|array] an array of buttons, for creating confirmation dialogs.
-      , animation: {
-          open:      {height: 'toggle'}  // or Animate.css class names like: 'animated bounceInLeft'
-          , close:   'animated flipOutY' // or Animate.css class names like: 'animated bounceOutLeft'
-          , easing:  'swing'
-          , speed:   500                 // opening & closing animation speed
+      layout: 'topRight'
+      ,
+      theme: 'defaultTheme'     // or relax
+      ,
+      type: 'success'          // alert, success, error, warning, information, notification
+      ,
+      text: ''                 // [string|html] can be HTML or STRING
+      ,
+      dismissQueue: true               // [boolean] If you want to use queue feature set this true
+      ,
+      force: false              // [boolean] adds notification to the beginning of queue when set to true
+      ,
+      maxVisible: 8                  // [integer] you can set max visible notification count for dismissQueue true option,
+      ,
+      template: '<div class="noty_message"><span class="noty_text"></span><div class="noty_close"></div></div>'
+      ,
+      timeout: 5000               // [integer|boolean] delay for closing event in milliseconds. Set false for sticky notifications
+      ,
+      progressBar: true               // [boolean] - displays a progress bar
+      ,
+      buttons: false              // [boolean|array] an array of buttons, for creating confirmation dialogs.
+      ,
+      animation: {
+        open: { height: 'toggle' }  // or Animate.css class names like: 'animated bounceInLeft'
+        , close: 'animated flipOutY' // or Animate.css class names like: 'animated bounceOutLeft'
+        , easing: 'swing'
+        , speed: 500                 // opening & closing animation speed
+      }
+      ,
+      closeWith: ['click'] // ['click', 'button', 'hover', 'backdrop']     // backdrop click will close all notifications
+      ,
+      modal: false     // [boolean] if true adds an overlay
+      ,
+      killer: false     // [boolean] if true closes all notifications and shows itself
+      ,
+      callback: {
+        onShow: function () {
         }
-      , closeWith:   ['click'] // ['click', 'button', 'hover', 'backdrop']     // backdrop click will close all notifications
-      , modal:       false     // [boolean] if true adds an overlay
-      , killer:      false     // [boolean] if true closes all notifications and shows itself
-      , callback: {
-          onShow:         function () {}
-          , afterShow:    function () {}
-          , onClose:      function () {}
-          , afterClose:   function () {}
-          , onCloseClick: function () {}
+        , afterShow: function () {
         }
+        , onClose: function () {
+        }
+        , afterClose: function () {
+        }
+        , onCloseClick: function () {
+        }
+      }
     };
   }());
 
@@ -219,6 +239,20 @@ window.jQuery(($) => {
     };
 
     fontawesome.library.add(faGrapQl);
+
+    var faSplat = {
+      prefix: 'fac',
+      iconName: 'splat',
+      icon: [
+        448, 448,
+        [],
+        null,
+        'M163.006,417.598 L166.015,306.629 L63.506,343.841 L129.87,255.871 L25.5,224.5 L129.87,193.129 L63.506,105.159 L166.015,142.371 L163.006,31.402 L224.5,122.983 L285.995,31.402 L282.985,142.371 L385.495,105.159 L319.13,193.13 L423.5,224.5 L319.13,255.871 L385.494,343.841 L282.984,306.629 L285.994,417.598 L224.5,326.017 z'
+      ]
+    };
+
+    fontawesome.library.add(faSplat);
+
     console.log('Added custom icons');
 
     // fontawesome.library.add(fas);
