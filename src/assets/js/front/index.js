d020f5f38763f3d7edac7bc3a350f79fc6c49904
Arsénio Miguel Santos Costa <arseniocosta@med.uminho.pt>
2019-08-21 12:20:57 +0100

    - [WIP] resumee

--- src/assets/js/front/index.js
+++ src/assets/js/front/index.js
@@ -59,7 +59,7 @@ window.jQuery(($) => {
   (function () {
 
     let w = window;
-    let dataRoot = w.location.origin + 'assets/data/';
+    let dataRoot = w.location.origin + '/assets/data/';
 
     // Examine the text in the response
     function status(r) {
